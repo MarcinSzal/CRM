ec74e88c0d006b19cc8effc2e70d0e762051159b
wbrzosko <wbrzosko@gmail.com>
2018-12-01 14:44:46 +0100

    Added ArrayList to Customer class

--- src/CRMproject/Customer.java
+++ src/CRMproject/Customer.java
@@ -1,19 +1,18 @@
 package CRMproject;
 
-
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Customer {
 
     private String name;
     private int number;
-
+    private List<Call> calls;
 
     public Customer(String name,int number){
         this.name = name;
         this.number = number;
-
-
+        this.calls = new ArrayList<Call>();
     }
 
     public String getName(){
@@ -24,14 +23,23 @@ public class Customer {
         return number;
     }
 
-
-
-
     public String toStringg(Customer customer){
         return customer.toString();
     }
 
     public static Customer addCustomer(String name, int number){
         return new Customer(name, number);
+    }
+
+    public List<Call> getCalls() {
+        return calls;
+    }
+
+    public void addCall (Call call) {
+        calls.add(call);
+    }
+
+    public void printAllCalls() {
+        calls.forEach(call -> System.out.println(call.toString()));
     }
 }
